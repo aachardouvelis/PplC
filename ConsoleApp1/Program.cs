8912f49c55dde47d2936fae7cd1c2f5db7b930f1
AlexChar <a.a.chardouvelis@gmail.com>
2020-07-28 20:24:15 +0300

    My third

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 			Console.WriteLine("Hi m8");
-            Console.Write("HAI");
+            Console.WriteLine("HAI HAI HAI");
         }
     }
 }
